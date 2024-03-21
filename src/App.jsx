99a4c8663885d2cbc7afcaba31d512d825c1ab5d
Nahuelhermosa <nahuel_pro_517@hotmail.com>
2024-03-20 22:44:47 -0300

    setLoading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { ClipLoader } from "react-spinners";
 import "./App.css";
 import Header from "./componente/Header"
 import ProductsList from "./componente/ProductsList";
@@ -6,20 +7,43 @@ import ProductsList from "./componente/ProductsList";
 
 function App() {
   const [products, setProducts] = useState([]);
-  useEffect(()=>{  
-   async function getData (endpoint) {
-    try {
-      const res = await fetch(endpoint);
-   const data = await res.json();
-   setProducts(data);
-    } catch (error) {
-      console.error(error.message);
-    }
+  const [isLoading, setIsLoading] = useState(true);
+
+  //useEffect(()=>{  
+  // async function getData (endpoint) {
+  //  try {
+  //    const res = await fetch(endpoint);
+  // const data = await res.json();
+  // setProducts(data);
+  //  } catch (error) {
+  //    console.error(error.message);
+  //  }
    
+  //}
+  //getData('https://fakestoreapi.com/products');
+  //}, []);
+
+   useEffect(()=>{
+    function getData(endpoint){
+      setIsLoading(true);
+      fetch(endpoint)
+        .then(res => res.json())
+        .then(data => setProducts(data))
+        .catch(error => cosole.error (error.message))
+        .finally(()=>setIsLoading(false));
+
   }
   getData('https://fakestoreapi.com/products');
-  }, []);
+  },[]);
+  
+  if(isLoading) return  <ClipLoader
+  color='tomato'
+  size={150}
+  aria-label="Loading Spinner"
+  data-testid="loader"
+/>
 
+  
   return (
     <>
      <Header/>
